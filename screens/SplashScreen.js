962949e6e8e2c4c83d456ba4c6f42b18b4c06d69
Nopparat Munsuwan <guydoy456@gmail.com>
2022-01-25 19:30:33 +0700

    update25/1/2022

--- screens/SplashScreen.js
+++ screens/SplashScreen.js
@@ -1,32 +1,32 @@
 import React from 'react';
-import { 
-    View, 
-    Text, 
-    TouchableOpacity, 
-    Dimensions,
-    StyleSheet,
-    StatusBar,
-    Image,
-    ImageBackground
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Dimensions,
+  StyleSheet,
+  StatusBar,
+  Image,
+  ImageBackground,
 } from 'react-native';
 
-
 const SplashScreen = ({navigation}) => {
-    
-    React.useEffect(()=>{
-      
-           setTimeout(()=> navigation.navigate('LoginScreen'),5000)
-      
-    },[])
+  React.useEffect(() => {
+    setTimeout(() => navigation.navigate('LoginScreen'), 5000);
+  }, []);
 
-    return (
-      <ImageBackground source={require('../assets/background.jpg')} resizeMode="cover" style={{flex:1,justifyContent:'center',alignItems:'center'}}>
-         <Image source={require('../assets/logo.png')}  resizeMode='contain' style={{width:250,height:250}}/>
-      </ImageBackground>
-    );
+  return (
+    <ImageBackground
+      source={require('../assets/background.jpg')}
+      resizeMode="cover"
+      style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Image
+        source={require('../assets/logo.png')}
+        resizeMode="contain"
+        style={{width: 250, height: 250}}
+      />
+    </ImageBackground>
+  );
 };
 
 export default SplashScreen;
-
-
-
